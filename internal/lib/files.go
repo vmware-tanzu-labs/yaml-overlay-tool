590d42a2e7a17837d214195eb956b5ae22ad0de8
Jeff Davis <jeffda@vmware.com>
2021-04-15 14:29:44 -0500

    feat: add file reader and closer
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

---
+++ internal/lib/files.go
@@ -0,0 +1,24 @@
+package lib
+
+import (
+	"bufio"
+	"io"
+	"os"
+
+	"log"
+)
+
+func ReadStream(fileName string) (io.Reader, error) {
+	if fileName == "-" {
+		return bufio.NewReader(os.Stdin), nil
+	}
+
+	return os.Open(fileName)
+}
+
+func CloseFile(file *os.File) {
+	err := file.Close()
+	if err != nil {
+		log.Printf("error closing file!: %s", err)
+	}
+}
