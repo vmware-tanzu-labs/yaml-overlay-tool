c455a986ac32265d848a010769f0bf15d77ac927
Andrew J. Huffman <ahuffman@vmware.com>
2021-05-03 16:58:20 -0400

    feat: add func to determine common path prefix
    Signed-off-by: Andrew J. Huffman <ahuffman@vmware.com>

--- internal/lib/files.go
+++ internal/lib/files.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
 )
 
 func ReadStream(fileName string) (io.Reader, error) {
@@ -27,4 +28,41 @@ func CloseFile(file *os.File) {
 	if err := file.Close(); err != nil {
 		log.Error("error closing file!: %s", err)
 	}
+}
+
+func GetCommonPrefix(sep byte, paths ...string) string {
+	switch len(paths) {
+	case 0:
+		return ""
+	case 1:
+		return path.Clean(paths[0])
+	}
+
+	c := path.Clean(paths[0]) + string(sep)
+
+	for _, v := range paths[1:] {
+		v = path.Clean(v) + string(sep)
+
+		if len(v) < len(c) {
+			c = c[:len(v)]
+		}
+
+		for i := 0; i < len(c); i++ {
+			if v[i] != c[i] {
+				c = c[:i]
+
+				break
+			}
+		}
+	}
+
+	for i := len(c) - 1; i >= 0; i-- {
+		if c[i] == sep {
+			c = c[:i]
+
+			break
+		}
+	}
+
+	return c
 }
