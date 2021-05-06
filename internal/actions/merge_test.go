4823fdb5e19aafdf3f398dbc87fc76d9cb8f7d2b
Andrew J. Huffman <ahuffman@vmware.com>
2021-05-05 20:20:16 -0400

    test: stub out merge and replace tests
    Signed-off-by: Andrew J. Huffman <ahuffman@vmware.com>

---
+++ internal/actions/merge_test.go
@@ -0,0 +1,6 @@
+// Copyright 2021 VMware, Inc.
+// SPDX-License-Identifier: MIT
+
+// contains tests for the yaml-overlay-tool 'merge' action
+
+package actions_test
---
+++ internal/actions/replace_test.go
@@ -0,0 +1,6 @@
+// Copyright 2021 VMware, Inc.
+// SPDX-License-Identifier: MIT
+
+// contains tests for the yaml-overlay-tool 'replace' action
+
+package actions_test
