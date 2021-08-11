e2ab14d92526fcd285008b1d319fa5ad0e53337a
Andrew J. Huffman <ahuffman@vmware.com>
2021-08-11 09:41:42 -0400

    docs(tutorials): Update setup script to move yamls in place
    Signed-off-by: Andrew J. Huffman <ahuffman@vmware.com>

--- docs/tutorials/katacoda/getting-started/assets/labsetup.sh
+++ docs/tutorials/katacoda/getting-started/assets/labsetup.sh
@@ -5,3 +5,5 @@ echo "Installing YAML Overlay Tool"
 curl -sL -o /tmp/yot_amd64.deb https://github.com/vmware-tanzu-labs/yaml-overlay-tool/releases/download/v0.6.3/yot_amd64.deb && sudo dpkg -i /tmp/yot_amd64.deb
 
 echo "YAML Overlay Tool installed successfully"
+
+mkdir ~/source-manifests && mv ~/*.yaml ~/source-manifests/
